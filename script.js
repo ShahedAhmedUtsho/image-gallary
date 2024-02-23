b6e4e10b758396e5b661a459b5d42241a512fb87
Shahed Ahmed <shahedahmedutsho@gmail.com>
2024-02-23 20:37:19 +0600

    first-commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let gridImg = document.querySelectorAll('.grid .gird-item img');
+
+
+for(let img of gridImg ){
+    
+    img.style.width="100%";
+    img.style.height="100%";
+    img.style.objectFit="cover"
+    img.style.backgroundColor="gray"
+    
+
+}
